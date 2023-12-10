f95ae267fc41e8a1759ae032228fc41c89899a51
Stephen Hutchings <stephen@s-ings.com>
2023-12-10 15:07:57 +1100

    More WIP

--- utils/is-valid.js
+++ utils/is-valid.js
@@ -6,9 +6,13 @@ export default (svg) => {
   if (/stroke|gradient/.test(svg)) return false
 
   // Only a limited number of elements may be used
-  if (/<(?!\/?(path|svg|g|use|circle|clipPath|defs)[ >])/g.test(svg)) {
+  if (/<(?!\/?(path|svg|g|use|circle|defs)[ >])/g.test(svg)) {
     return false
   }
+
+  // Flags should match the standard proportions,
+  // with the exception of for Switzerland and Nepal
+  if (!/<svg[^>]+width="(1200|720|591)" height="720"/.test(svg)) return false
 
   return true
 }
